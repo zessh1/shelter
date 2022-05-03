b77491f02bedc580e7152a373ad5a23def9da99e
Giorgi <giorgi.mamulashvili2003@gmail.com>
2022-05-03 17:23:17 +0400

    fix: burger menu fix: asserts feat: <a> tag hrefs

--- pages/main/script.js
+++ pages/main/script.js
@@ -29,10 +29,20 @@ function return_menu() {
     element.classList.remove("active");
     element_two.classList.remove("active");
 }
-const burgerMenu = document.getElementById('burgerLogo');
 
+
+const burgerMenu = document.getElementById('burgerLogo');
 burgerMenu.onclick = function () {
     document.querySelector('.burger').classList.add('active');
     element.classList.add("active");
+    document.getElementById("logo").style.display = "none";
+
+}
+const burgerExit = document.getElementById('burger-exit-menu')
+burgerExit.onclick = function () {
+    document.querySelector('.burger').classList.remove('active');
+    element.classList.remove("active");
+    document.getElementById("logo").style.display = "flex";
+    document.getElementById("logo").style.flexDirection = "column";
 }
 
