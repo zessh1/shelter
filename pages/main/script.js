4e834074eda00f5805946a641cff973a77ae1f08
Giorgi <giorgi.mamulashvili2003@gmail.com>
2022-05-06 01:39:17 +0400

    fix: margin,padding pixels feat: add our page html css fix: links feat: add fonts

--- pages/main/script.js
+++ pages/main/script.js
@@ -1,3 +1,38 @@
+sliderPopInfo = {
+    jennifer: {
+        title: 'Jennifer',
+        descrTitle: 'Dog - Labrador',
+        descr: 'Jennifer is a sweet 2 months old Labrador that is patiently waiting to find a new forever home. This girl really enjoys being able to go outside to run and play, but won\'t hesitate to play up a storm in the house if she has all of her favorite toys.',
+        age: '2 month',
+        inoculations: 'none',
+        disiases: 'none',
+        parasites: '3',
+        petImg: '../../assets/images/pets-jennifer.png'
+    },
+     katarine: {
+        title: 'Katrine',
+        descrTitle: 'Cat - British',
+        descr: 'Katrine is a sweet 2 months old Cat that is patiently waiting to find a new forever home. This girl really enjoys being able to go outside to run and play, but won\'t hesitate to play up a storm in the house if she has all of her favorite toys.',
+        age: '5 month',
+        inoculations: 'none',
+        disiases: 'none',
+        parasites: 'none',
+        petImg: '../../assets/images/pets-katrine.png'
+    },
+     woody: {
+        title: 'Woody',
+        descrTitle: 'dog - Golden',
+        descr: 'Woody is a sweet 2 months old Dog that is patiently waiting to find a new forever home. This girl really enjoys being able to go outside to run and play, but won\'t hesitate to play up a storm in the house if she has all of her favorite toys.',
+        age: '3 month',
+        inoculations: 'none',
+        disiases: 'none',
+        parasites: '2',
+        petImg: '../../assets/images/pets-woody.png'
+    },
+}
+
+
+
 const slideLeft = document.getElementById('slideLeft');
 const slideRight = document.getElementById('slideRight');
 
@@ -18,10 +53,42 @@ slideLeftMobile.onclick = function () {
 };
     var element = document.getElementById('overlay');
     var element_two = document.querySelector('.slider-popup')
-function myfun() {
-    console.log(element_two)
+function callSliderPop(name) {
+    switch(name) {
+        case 'woody':
+            addInfoToPop(name);
+            break;
+        case 'jennifer':
+            addInfoToPop(name);
+            break;
+        case 'katarine':
+            addInfoToPop(name);
+            break;
+        default:
+
+    }
+    console.log('sliderPopInfo')
     element.classList.add("active");
     element_two.classList.add("active");
+}
+function addInfoToPop(name) {
+    const petName = document.querySelector('.pet-name');
+    const petOrigin = document.querySelector('.pet-origin');
+    const petDescr = document.querySelector('.pet-descr');
+    const petAge = document.querySelector('.pet-age');
+    const petInoculations = document.querySelector('.pet-inoculations');
+    const petDiseases = document.querySelector('.pet-diseases');
+    const petParasites = document.querySelector('.pet-parasites');
+    const petImg = document.querySelector('.about-pets-img')
+
+    petName.innerHTML = sliderPopInfo[name].title
+    petOrigin.innerHTML = sliderPopInfo[name].descrTitle
+    petDescr.innerHTML = sliderPopInfo[name].descr
+    petAge.innerHTML = sliderPopInfo[name].age
+    petInoculations.innerHTML = sliderPopInfo[name].inoculations
+    petDiseases.innerHTML = sliderPopInfo[name].disiases
+    petParasites.innerHTML = sliderPopInfo[name].parasites
+    petImg.src = sliderPopInfo[name].petImg
 }
 function return_menu() {
     var element = document.getElementById('overlay');
