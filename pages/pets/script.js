e47cd4d77b91984e99dc7a3dda1f840c49a4beb5
Giorgi <giorgi.mamulashvili2003@gmail.com>
2022-06-06 04:22:11 +0400

    fix: pets page burger menu click header display none

--- pages/pets/script.js
+++ pages/pets/script.js
@@ -1,14 +1,12 @@
 const burgerMenu = document.getElementById('burgerLogo');
 burgerMenu.onclick = function () {
     document.querySelector('.burger').classList.add('active');
-    element.classList.add("active");
     document.getElementById("logo").style.display = "none";
 
 }
 const burgerExit = document.getElementById('burger-exit-menu')
 burgerExit.onclick = function () {
     document.querySelector('.burger').classList.remove('active');
-    element.classList.remove("active");
     document.getElementById("logo").style.display = "flex";
     document.getElementById("logo").style.flexDirection = "column";
 }
