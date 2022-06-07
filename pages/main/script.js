643d2c956470bfd3ed227752222217fbd694aa3e
Giorgi <giorgi.mamulashvili2003@gmail.com>
2022-06-07 15:51:50 +0400

    add: dog information with API creat: pagination,burgermenu,...

--- pages/main/script.js
+++ pages/main/script.js
@@ -1,34 +1,124 @@
-sliderPopInfo = {
-    jennifer: {
-        title: 'Jennifer',
-        descrTitle: 'Dog - Labrador',
-        descr: 'Jennifer is a sweet 2 months old Labrador that is patiently waiting to find a new forever home. This girl really enjoys being able to go outside to run and play, but won\'t hesitate to play up a storm in the house if she has all of her favorite toys.',
-        age: '2 month',
-        inoculations: 'none',
-        disiases: 'none',
-        parasites: '3',
-        petImg: '../../assets/images/pets-jennifer.png'
-    },
-     katarine: {
-        title: 'Katrine',
-        descrTitle: 'Cat - British',
-        descr: 'Katrine is a sweet 2 months old Cat that is patiently waiting to find a new forever home. This girl really enjoys being able to go outside to run and play, but won\'t hesitate to play up a storm in the house if she has all of her favorite toys.',
-        age: '5 month',
-        inoculations: 'none',
-        disiases: 'none',
-        parasites: 'none',
-        petImg: '../../assets/images/pets-katrine.png'
-    },
-     woody: {
-        title: 'Woody',
-        descrTitle: 'dog - Golden',
-        descr: 'Woody is a sweet 2 months old Dog that is patiently waiting to find a new forever home. This girl really enjoys being able to go outside to run and play, but won\'t hesitate to play up a storm in the house if she has all of her favorite toys.',
-        age: '3 month',
-        inoculations: 'none',
-        disiases: 'none',
-        parasites: '2',
-        petImg: '../../assets/images/pets-woody.png'
-    },
+const bigData = []
+const data_copy = []
+fetch('../../tsconfig.json') //path to the file with json data
+    .then(response => {
+        return response.json();
+    })
+    .then(data => {
+        console.log(data);
+        paginationDataGenerator(48, 6, data)
+
+        const dataLength = data.length - 1;
+        for (var i = 0; i < data.length; i++) {
+            x = getRandomNumberBetween(0, 7);
+            data_copy.push(data[i])
+            const div = document.createElement("div")
+            const img = document.createElement("img")
+            const p = document.createElement("p")
+            const btn = document.createElement("button")
+            const slider_view_container = document.querySelector(".slider-view-container")
+
+            div.classList.add("slider-view");
+            img.classList.add("slider-view-img");
+            p.classList.add("slider-view-name");
+            btn.classList.add("learn-more", "btn");
+            btn.innerHTML = "Learn more";
+
+            img.src = data[x].img;
+            p.innerHTML = data[x].name;
+            console.log(data[x].name);
+
+            (function (index) {
+                btn.addEventListener("click", function () {
+                    body.style.overflow = "hidden"
+                    element.classList.add("active");
+
+                    addInfoToPop(index)
+
+                })
+            })(x);
+            div.appendChild(img);
+            div.appendChild(p);
+            div.appendChild(btn);
+            slider_view_container.appendChild(div);
+            console.log(dataLength);
+
+
+        }
+
+
+    });
+
+
+const body = document.querySelector("body")
+
+function addInfoToPop(k) {
+    const petName = document.querySelector('.pet-name');
+    const petOrigin = document.querySelector('.pet-origin');
+    const petBreed = document.querySelector('.breed');
+    const petDescr = document.querySelector('.pet-descr');
+    const petAge = document.querySelector('.pet-age');
+    const petInoculations = document.querySelector('.pet-inoculations');
+    const petDiseases = document.querySelector('.pet-diseases');
+    const petParasites = document.querySelector('.pet-parasites');
+    const petImg = document.querySelector('.about-pets-img')
+
+    petName.innerHTML = data_copy[k].name
+    petOrigin.innerHTML = data_copy[k].type
+    petBreed.innerHTML = data_copy[k].breed
+    petDescr.innerHTML = data_copy[k].description
+    petAge.innerHTML = data_copy[k].age
+    petInoculations.innerHTML = data_copy[k].inoculations
+    petDiseases.innerHTML = data_copy[k].disiases
+    petParasites.innerHTML = data_copy[k].parasites
+    petImg.src = data_copy[k].img
+    element_two.classList.add('active')
+}
+
+function paginationDataGenerator(amount, count, data) {
+    // console.log(getRandomNumberBetweenPag(0, 6), 'asdasda');
+    for (let i = 0; i < amount; i++) {
+        bigData.push(data[getRandomNumberBetweenPag(0, count)])
+        console.log('aee' , bigData)
+    }
+}
+
+let lis = [];
+
+function getRandomNumberBetween(min, max) {
+    number = Math.floor(Math.random() * (max - min + 1) + min);
+
+    if (!lis.includes(number)) {
+        lis.push(number)
+        return number;
+    } else {
+        if (lis.length <= max) {
+            return getRandomNumberBetween(min, max)
+        } else {
+            console.log("amoiwura")
+            lis = [];
+            return false;
+        }
+    }
+
+}
+function getRandomNumberBetweenPag(min, max) {
+    let liss = []
+    number = Math.floor(Math.random() * (max - min + 1) + min);
+
+    if (!liss.includes(number)) {
+        liss.push(number)
+        return number;
+    } else {
+        if (liss.length <= max) {
+            return getRandomNumberBetween(min, max)
+        } else {
+            console.log("amoiwura")
+            liss = [];
+            return false;
+        }
+    }
+
 }
 
 
@@ -51,50 +141,15 @@ slideRightMobile.onclick = function () {
 slideLeftMobile.onclick = function () {
     document.getElementById('sliderView').scrollLeft -= 220;
 };
-    var element = document.getElementById('overlay');
-    var element_two = document.querySelector('.slider-popup')
-function callSliderPop(name) {
-    switch(name) {
-        case 'woody':
-            addInfoToPop(name);
-            break;
-        case 'jennifer':
-            addInfoToPop(name);
-            break;
-        case 'katarine':
-            addInfoToPop(name);
-            break;
-        default:
+var element = document.getElementById('overlay');
+var element_two = document.querySelector('.slider-popup')
 
-    }
-    console.log('sliderPopInfo')
-    element.classList.add("active");
-    element_two.classList.add("active");
-}
-function addInfoToPop(name) {
-    const petName = document.querySelector('.pet-name');
-    const petOrigin = document.querySelector('.pet-origin');
-    const petDescr = document.querySelector('.pet-descr');
-    const petAge = document.querySelector('.pet-age');
-    const petInoculations = document.querySelector('.pet-inoculations');
-    const petDiseases = document.querySelector('.pet-diseases');
-    const petParasites = document.querySelector('.pet-parasites');
-    const petImg = document.querySelector('.about-pets-img')
-
-    petName.innerHTML = sliderPopInfo[name].title
-    petOrigin.innerHTML = sliderPopInfo[name].descrTitle
-    petDescr.innerHTML = sliderPopInfo[name].descr
-    petAge.innerHTML = sliderPopInfo[name].age
-    petInoculations.innerHTML = sliderPopInfo[name].inoculations
-    petDiseases.innerHTML = sliderPopInfo[name].disiases
-    petParasites.innerHTML = sliderPopInfo[name].parasites
-    petImg.src = sliderPopInfo[name].petImg
-}
 function return_menu() {
     var element = document.getElementById('overlay');
     var element_two = document.querySelector('.slider-popup')
     element.classList.remove("active");
     element_two.classList.remove("active");
+    body.style.overflow = "unset"
 }
 
 
@@ -113,3 +168,7 @@ burgerExit.onclick = function () {
     document.getElementById("logo").style.flexDirection = "column";
 }
 
+function hideburger() {
+    document.querySelector('.burger').classList.remove('active');
+    element.classList.remove("active");
+}
--- pages/pets/script.js
+++ pages/pets/script.js
@@ -1,12 +1,306 @@
 const burgerMenu = document.getElementById('burgerLogo');
+const pagination_right_jump = document.getElementById("pagination-right-jump")
+const pagination_right_full_jump = document.getElementById("pagination-right-full-jump")
+const pagination_left_jump = document.getElementById("pagination-left-jump")
+const pagination_left_full_jump = document.getElementById("pagination-left-full-jump")
+const pagination_page = document.getElementById("pagination-page")
+
+let bigData = []
+var element = document.getElementById('overlay');
 burgerMenu.onclick = function () {
     document.querySelector('.burger').classList.add('active');
+    element.classList.add('active')
     document.getElementById("logo").style.display = "none";
 
 }
 const burgerExit = document.getElementById('burger-exit-menu')
 burgerExit.onclick = function () {
     document.querySelector('.burger').classList.remove('active');
+    element.classList.remove('active')
     document.getElementById("logo").style.display = "flex";
     document.getElementById("logo").style.flexDirection = "column";
 }
+
+function hideburger() {
+    document.querySelector('.burger').classList.remove('active');
+    element.classList.remove('active')
+}
+fetch('../../tsconfig.json') //path to the file with json data
+    .then(response => {
+        return response.json();
+    })
+    .then(data => {
+
+        paginationDataGenerator(48, 7, data)
+
+    });
+function paginationDataGenerator(amount, count, data) {
+
+    for (let i = 0; i < amount; i++) {
+        bigData.push(data[getRandomNumberBetweenPag(0, count)])
+
+    }
+    checkdisablebtn();
+    if(window.screen.availWidth >= 320) {
+        drawpets(0,3)
+        console.log(window.screen.availWidth)
+        console.log(">780")
+    }
+    if( window.screen.availWidth >= 780) {
+        drawpets(0,6)
+        console.log(window.screen.availWidth)
+        console.log("780< x >1280")
+    }
+    if(window.screen.availWidth >= 1280) {
+        drawpets(0,8)
+        console.log(window.screen.availWidth)
+        console.log("1280>")
+    }
+}
+function drawpets( startCount = 0,count ){
+        const slider_view_container = document.querySelector(".our-pets-grid-box")
+        slider_view_container.innerHTML = '';
+    for (var i = startCount; i < count; i++) {
+        const div = document.createElement("div")
+        const img = document.createElement("img")
+        const p = document.createElement("p")
+        const btn = document.createElement("button")
+
+        div.classList.add("slider-view");
+        img.classList.add("slider-view-img");
+        p.classList.add("slider-view-name");
+        btn.classList.add("learn-more", "btn");
+        btn.innerHTML = "Learn more";
+
+        img.src = bigData[i].img;
+        p.innerHTML = bigData[i].name;
+
+
+        (function (index) {
+            btn.addEventListener("click", function () {
+                body.style.overflow = "hidden"
+                element.classList.add("active");
+
+                addInfoToPop(index)
+
+            })
+        })(i);
+        div.appendChild(img);
+        div.appendChild(p);
+        div.appendChild(btn);
+        slider_view_container.appendChild(div);
+
+
+    }
+}
+
+// let haveIt = [];
+//
+// function generateUniqueRandom(maxNr) {
+//
+//     let random = (Math.random() * maxNr).toFixed();
+//
+//
+//     random = Number(random);
+//
+//     if(!haveIt.includes(random)) {
+//         haveIt.push(random);
+//         return random;
+//     } else {
+//         if(haveIt.length < maxNr) {
+//
+//             haveIt = []
+//             return  generateUniqueRandom(maxNr);
+//         }
+//     }
+// }
+
+function getRandomNumberBetweenPag(min, max) {
+    let liss = []
+    number = Math.floor(Math.random() * (max - min + 1) + min);
+
+    if (!liss.includes(number)) {
+        liss.push(number)
+        return number;
+    } else {
+        if (liss.length <= max) {
+            return getRandomNumberBetween(min, max)
+        } else {
+
+            liss = [];
+            return false;
+        }
+    }
+
+}
+
+let startnum = 0
+let lastnum = 8
+
+if (window.screen.availWidth >= 320) {
+    lastnum = 3
+    console.log(lastnum, "asdasdas", window.screen.availWidth)
+
+}if (window.screen.availWidth >= 780) {
+    lastnum = 6
+
+}if (window.screen.availWidth >= 1280) {
+    lastnum = 8
+
+}
+
+function checkdisablebtn() {
+    if (lastnum >= bigData.length) {
+        console.log('ss', bigData.length)
+        pagination_right_jump.disabled = true;
+        pagination_right_full_jump.disabled = true;
+        pagination_left_jump.disabled = false;
+        pagination_left_full_jump.disabled = false;
+        pagination_right_jump.classList.remove("btn-clickable")
+        pagination_right_full_jump.classList.remove("btn-clickable")
+        pagination_left_jump.classList.add("btn-clickable")
+        pagination_left_full_jump.classList.add("btn-clickable")
+    }
+    else if (startnum <= 0) {
+        console.log('dd')
+        pagination_left_jump.disabled = true;
+        pagination_left_full_jump.disabled = true;
+        pagination_right_jump.disabled = false;
+        pagination_right_full_jump.disabled = false;
+        pagination_right_jump.classList.add("btn-clickable")
+        pagination_right_full_jump.classList.add("btn-clickable")
+        pagination_left_jump.classList.remove("btn-clickable")
+        pagination_left_full_jump.classList.remove("btn-clickable")
+
+    }
+    else {
+        console.log('ae')
+        pagination_left_jump.disabled = false;
+        pagination_left_full_jump.disabled = false;
+        pagination_right_jump.disabled = false;
+        pagination_right_full_jump.disabled = false;
+        pagination_right_jump.classList.add("btn-clickable")
+        pagination_right_full_jump.classList.add("btn-clickable")
+        pagination_left_jump.classList.add("btn-clickable")
+        pagination_left_full_jump.classList.add("btn-clickable")
+    }
+}
+
+pagination_right_jump.onclick = function () {
+    if(window.screen.availWidth >= 1280) {
+        startnum += 8
+        lastnum += 8
+        drawpets(startnum,lastnum)
+        checkdisablebtn()
+        pagination_page.innerHTML = (lastnum / 8).toString()
+        console.log(lastnum,"ssaa")
+        return;
+    }
+    if (window.screen.availWidth >= 780) {
+        startnum += 6
+        lastnum += 6
+        drawpets(startnum,lastnum)
+        checkdisablebtn()
+        pagination_page.innerHTML = (lastnum / 6).toString()
+        console.log(lastnum,"6saa")
+        return;
+    }
+    if (window.screen.availWidth >= 320) {
+        startnum += 3
+        lastnum += 3
+        drawpets(startnum,lastnum)
+        checkdisablebtn()
+        pagination_page.innerHTML = (lastnum / 3).toString()
+        console.log(lastnum,"3saa")
+        return;
+    }
+
+}
+pagination_right_full_jump.onclick = function () {
+    if(window.screen.availWidth >= 1280) {
+        startnum = 40
+        lastnum = 48
+        drawpets(startnum,lastnum)
+        checkdisablebtn()
+        pagination_page.innerHTML = (lastnum / 8).toString()
+        return;
+    }
+    if (window.screen.availWidth >= 780) {
+        startnum = 42
+        lastnum = 48
+        drawpets(startnum,lastnum)
+        checkdisablebtn()
+        pagination_page.innerHTML = (lastnum / 6).toString()
+        return;
+    }
+    if (window.screen.availWidth >= 320) {
+        startnum = 45
+        lastnum = 48
+        drawpets(startnum,lastnum)
+        checkdisablebtn()
+        pagination_page.innerHTML = (lastnum / 3).toString()
+        return;
+    }
+}
+
+pagination_left_jump.onclick = function () {
+    if(window.screen.availWidth >= 1280) {
+
+        startnum -= 8
+        lastnum -= 8
+        drawpets(startnum,lastnum)
+        checkdisablebtn()
+        pagination_page.innerHTML = (lastnum / 8).toString()
+        return;
+    }
+    if (window.screen.availWidth >= 780) {
+
+        startnum -= 6
+        lastnum -= 6
+        drawpets(startnum,lastnum)
+        checkdisablebtn()
+        pagination_page.innerHTML = (lastnum / 6).toString()
+        return
+    }
+    if (window.screen.availWidth >= 320) {
+
+        startnum -= 3
+        lastnum -= 3
+        drawpets(startnum,lastnum)
+        checkdisablebtn()
+        pagination_page.innerHTML = (lastnum / 3).toString()
+        console.log(lastnum,"4saa")
+        return;
+    }
+
+}
+pagination_left_full_jump.onclick = function () {
+    if(window.screen.availWidth >= 1280) {
+
+        startnum = 0
+        lastnum = 8
+        drawpets(startnum,lastnum)
+        checkdisablebtn()
+        pagination_page.innerHTML = (lastnum / 8).toString()
+        return;
+    }
+    if(window.screen.availWidth >= 780) {
+
+        startnum = 0
+        lastnum = 6
+        drawpets(startnum,lastnum)
+        checkdisablebtn()
+        pagination_page.innerHTML = (lastnum / 6).toString()
+        return;
+    }
+    if(window.screen.availWidth >= 320) {
+
+        startnum = 0
+        lastnum = 3
+        drawpets(startnum,lastnum)
+        checkdisablebtn()
+        pagination_page.innerHTML = (lastnum / 3).toString()
+        return;
+    }
+}
+
